b0b6b186f4d63281ab4d9a6d783ef5854a95853d
Ryan Kaskel <dev@ryankaskel.com>
2025-06-19 07:50:22 +0000

    Chapter 27: Thread API

--- threads-api/main-race.c
+++ threads-api/main-race.c
@@ -3,16 +3,21 @@
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    Pthread_mutex_lock(&m);
+    balance++; // unprotected access
+    Pthread_mutex_unlock(&m);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+    Pthread_mutex_lock(&m);
     balance++; // unprotected access
+    Pthread_mutex_unlock(&m);
     Pthread_join(p, NULL);
     return 0;
 }
